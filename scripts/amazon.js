45514e6f62bebfe81eec7eb90559aec6cf161d24
RohitSett24 <rohitsett4002@gmail.com>
2026-03-24 19:11:22 +0530

    adding to cart using the value in the quanity selector

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -25,7 +25,7 @@ products.forEach((product, index) => {
       </div>
 
       <div class="product-quantity-container">
-        <select>
+        <select class = "js-quantity-selector-${product.id}">
           <option selected value="1">1</option>
           <option value="2">2</option>
           <option value="3">3</option>
@@ -60,6 +60,11 @@ document.querySelectorAll('.js-add-to-cart-btn')
     button.addEventListener('click', () => {
       const productId = button.dataset.productId;
       
+      const selectElement = document.querySelector(`.js-quantity-selector-${productId}`);
+      const quantitySelected = Number(selectElement.value);
+
+      console.log(quantitySelected);
+
       let matchingItem;
       
       cart.forEach((item) => {
@@ -69,11 +74,11 @@ document.querySelectorAll('.js-add-to-cart-btn')
       });
 
       if(matchingItem) {
-        matchingItem.quantity += 1;
+        matchingItem.quantity += quantitySelected;
       } else {
         cart.push({
           productId: productId,
-          quantity: 1
+          quantity: quantitySelected
         })
       }
 
