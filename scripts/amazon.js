a818df2a464466d4e83856724bf2778227d25bd2
RohitSett24 <rohitsett4002@gmail.com>
2026-03-24 11:53:02 +0530

    make cart Quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -76,6 +76,17 @@ document.querySelectorAll('.js-add-to-cart-btn')
           quantity: 1
         })
       }
+
+      let cartQuantity = 0;
+      
+      cart.forEach((item) => {
+        cartQuantity += item.quantity;
+      });
+
+      document.querySelector('.js-cart-quantity')
+        .innerHTML = cartQuantity;
+
+      console.log(cartQuantity);
       console.log(cart); 
     });
   });
